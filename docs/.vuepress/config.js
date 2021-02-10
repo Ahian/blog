e98d54c9e57f91a306f2175083092e049cfd71bd
songyuehang <songyuehang@bytedance.com>
2021-02-10 23:05:46 +0800

    giao

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: '学习笔记',
+    description: '知识点记录',
+    head: [
+        ['link', { rel: 'icon', href: '/img/favicon.ico' }]
+      ],
+    themeConfig: {
+        logo: '/img/novel.png',
+      // 	在下方修改添加
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+          ]
+      // 	在上方修改添加
+      },
+      base:'blog'
+  }
